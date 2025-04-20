669036fc0aa79845ef58a25e4e9beeec9a815994
Slyyxz-uwu <Slyyxz-uwu>
2025-04-20 14:29:18 +0100

    Working hand animations

--- Assets/Hand_Animator.cs
+++ Assets/Hand_Animator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
@@ -10,13 +11,18 @@ public class Hand_Animator : MonoBehaviour
 
     [SerializeField] private NearFarInteractor nearFarInteractor;
     [SerializeField] private SkinnedMeshRenderer handMesh;
+    [SerializeField] private InputActionReference selectActionRef;
+    [SerializeField] private InputActionReference activateActionRef;
+    [SerializeField] private Animator handAnimator;
+
+    private static readonly int activateAmin = Animator.StringToHash("activate");
+    private static readonly int selectAmin = Animator.StringToHash("select");
+
 
     private void Awake()
     {
         nearFarInteractor.selectEntered.AddListener(OnGrab);
         nearFarInteractor.selectExited.AddListener(OnRelease);
-
-
     }
 
     private void OnGrab(SelectEnterEventArgs args)
@@ -34,5 +40,12 @@ public class Hand_Animator : MonoBehaviour
         {
             handMesh.enabled = true; // Show the hand again when released
         }
+    }
+
+    private void Update()
+    {
+        handAnimator.SetFloat(activateAmin, activateActionRef.action.ReadValue<float>());
+        handAnimator.SetFloat(selectAmin, selectActionRef.action.ReadValue<float>());
+
     }
 }
